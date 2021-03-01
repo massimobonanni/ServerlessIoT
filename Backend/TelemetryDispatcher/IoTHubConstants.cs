0f306d624b659745230bfa1df44455237182ec60
Massimo Bonanni <massimo.bonanni@tiscali.it>
2021-03-01 18:18:14 +0100

    Add Auto fill for device telemetry

---
+++ Backend/TelemetryDispatcher/IoTHubConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetryDispatcher
+{
+    public static class IoTHubConstants
+    {
+        public const string ConnectionDeviceID = "iothub-connection-device-id";
+        public const string EnqueuedTime = "iothub-enqueuedtime";
+    }
+}
--- Backend/TelemetryDispatcher/ProcessEventArgsExtensions.cs
+++ Backend/TelemetryDispatcher/ProcessEventArgsExtensions.cs
@@ -1,8 +1,11 @@
 ﻿using Newtonsoft.Json;
 using ServerlessIoT.Core;
+using ServerlessIoT.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using TelemetryDispatcher;
 
 namespace Azure.Messaging.EventHubs.Processor
 {
@@ -15,6 +18,25 @@ namespace Azure.Messaging.EventHubs.Processor
             {
                 string data = Encoding.UTF8.GetString(@event.Data.Body.ToArray());
                 deviceTelemetry = JsonConvert.DeserializeObject<DeviceTelemetry>(data);
+                if (deviceTelemetry != null && string.IsNullOrWhiteSpace(deviceTelemetry.DeviceId))
+                {
+                    if (@event.Data.SystemProperties.ContainsKey(IoTHubConstants.ConnectionDeviceID))
+                    {
+                        deviceTelemetry.DeviceId = @event.Data.SystemProperties[IoTHubConstants.ConnectionDeviceID]?.ToString();
+                        deviceTelemetry.DeviceName = @event.Data.SystemProperties[IoTHubConstants.ConnectionDeviceID]?.ToString();
+                    }
+                }
+                if (deviceTelemetry != null && deviceTelemetry.Timestamp == default)
+                {
+                    if (@event.Data.SystemProperties.ContainsKey(IoTHubConstants.EnqueuedTime))
+                    {
+                        if (DateTimeOffset.TryParse(@event.Data.SystemProperties[IoTHubConstants.EnqueuedTime]?.ToString(),
+                            null, DateTimeStyles.AssumeUniversal, out var enqueueTime))
+                        {
+                            deviceTelemetry.Timestamp = enqueueTime;
+                        }
+                    }
+                }
             }
             catch (Exception)
             {
